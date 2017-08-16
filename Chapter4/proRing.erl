d45bb210fed21803eba09769a25c8301fb339667
danieljharris <danieljharris26@yahoo.co.uk>
2017-08-16 16:34:12 +0100

    Update proRing.erl

--- Chapter4/proRing.erl
+++ Chapter4/proRing.erl
@@ -1,8 +1,8 @@
 -module(proRing).
 % -export([ring_start/2]).
 % -export([ring_loop/4]).
--export([ring/3]).
--export([ring/4, ring_loop/3]).
+-export([start/1, stop/0, message/2]).
+-export([ring_loop/1]).
 
 %%Exercise 4-2: The Process Ring
 %Not sure what was meant by 'number of messages'
@@ -22,30 +22,64 @@
 
 
 %%Exercise 4-2: The Process Ring (Updated)
-% The first process will send the specified amount of messages
-% It seems to break sometimes when its run? But runs as it should most of the time
-ring(Length, M, Message) -> ring(0, Length, M, Message).
+start(Length) ->
+	register(head, spawn(proRing, ring_loop, [0])),
+	head!{start, Length-1}.
 
-ring(F_Pid, Length, M, Message) when Length == 1 -> F_Pid!{message, Message}, ring_loop(false, F_Pid, M);
-ring(F_Pid, Length, M, Message) when F_Pid  == 0 -> Pid = spawn(proRing, ring, [self(), Length -1, M, Message]), ring_loop(true, Pid, M);
-ring(F_Pid, Length, M, Message) when F_Pid  /= 0 -> Pid = spawn(proRing, ring, [F_Pid,  Length -1, M, Message]), ring_loop(false, Pid, M).
+message(Message, Ammount) -> head!{message, Message, Ammount}.
 
+stop() -> head!stop.
 
-ring_loop(First, Pid, M) ->
+ring_loop(Pid) ->
 	receive
-		{message, _} when M == 0 -> catch Pid!stop;
+		{start, Length} ->
+			NewPid = spawn(proRing, ring_loop, [0]),
+			NewPid!{next, Length},
+			io:format(  "I'm: ~p, I'm Head, I point at: ~p~n", [ pid_to_list( self() ), NewPid ]  ),
+			ring_loop(NewPid);
 
-		{message, Message} when First ->
-			io:format("I'm: ~p, I point at: ~p~n", [pid_to_list(self()), pid_to_list(Pid)]),
-			Pid!{message, Message},
-			ring_loop(First, Pid, M-1);
+		{next, 1} ->
+			io:format(  "I'm: ~p, I'm Tail, I point at: ~p~n", [ pid_to_list( self() ), whereis(head) ]  ),
+			ring_loop(whereis(head));
 
-		{message, Message} when not First ->
-			io:format("I'm: ~p, and I got the message: ~p~n", [pid_to_list(self()), Message]),
-			Pid!{message, Message},
-			ring_loop(First, Pid, M);
+		{next, Length} ->
+			NewPid = spawn(proRing, ring_loop, [0]),
+			NewPid!{next, Length-1},
+			io:format(  "I'm: ~p, I'm Body, I point at: ~p~n", [ pid_to_list( self() ), NewPid ]  ),
+			ring_loop(NewPid);
 
-		stop -> catch Pid!stop, ok
+		{message, _Message, 0} -> ring_loop(Pid);
 
-	after 1000 -> timeout
+		% Recursive message sent accross the ring, if its the tail (the one that points to head) it decreases ammount
+		{message, Message, Ammount} ->
+			case whereis(head) of
+				Pid ->
+					io:format("I'm : ~p, I point at: ~p, I got the message: ~p~n", [pid_to_list(self()), pid_to_list(Pid), Message]),
+					Pid!{message, Message, Ammount-1},
+					ring_loop(Pid);
+				_Other ->
+					io:format("I'm: ~p, I point at: ~p, I got the message: ~p~n", [pid_to_list(self()), pid_to_list(Pid), Message]),
+					Pid!{message, Message, Ammount},
+					ring_loop(Pid)
+			end;
+
+		stop -> catch Pid!stop
 	end.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
