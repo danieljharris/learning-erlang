78870d3130747ed00b69aa51f648caeae2afd63e
danieljharris <danieljharris26@yahoo.co.uk>
2017-08-17 10:02:44 +0100

    Improved code & Implemented missing feature
    Made the deallocate only work if the same pid as it was allocated to

--- Chapter5/changeFreq.erl
+++ Chapter5/changeFreq.erl
@@ -34,7 +34,6 @@ call(Message) ->
 
 
 
-
 %% The Main Loop
 loop(Frequencies) ->
 	receive
@@ -44,18 +43,27 @@ loop(Frequencies) ->
 			loop(NewFrequencies);
 
 		{request, Pid , {deallocate, Freq}} ->
-			{NewFrequencies, Reply} = deallocate(Frequencies, Freq),
+			{NewFrequencies, Reply} = deallocate(Frequencies, Freq, Pid),
 			reply(Pid, Reply),
 			loop(NewFrequencies);
 
 		{request, Pid, stop} ->
-			{_, Allocated} = Frequencies,
-			case Allocated == [] of
-				true -> reply(Pid, ok);
-				false ->
-					reply(Pid, {error, frequencies_still_allocated}),
-					loop(Frequencies)
+			case Frequencies of
+			    {_, [] = _Allocated} ->
+			        reply(Pid, ok);
+			    {_, _Allocated} ->
+			        reply(Pid, {error, frequencies_still_allocated}),
+			        loop(Frequencies)
 			end
+
+		% {request, Pid, stop} ->
+			% {_, Allocated} = Frequencies,
+			% case Allocated == [] of
+			% 	true -> reply(Pid, ok);
+			% 	false ->
+			% 		reply(Pid, {error, frequencies_still_allocated}),
+			% 		loop(Frequencies)
+			% end
 	end.
 
 reply(Pid, Reply) -> Pid ! {reply, Reply}.
@@ -64,25 +72,37 @@ reply(Pid, Reply) -> Pid ! {reply, Reply}.
 
 %% The Internal Help Functions used to allocate and
 %% deallocate frequencies.
-allocate({[], Allocated}, _Pid) ->
-	{{[], Allocated}, {error, no_frequency}};
+allocate({[] = _Free, _Allocated} = Frequencies, _Pid) ->
+    { Frequencies, {error, no_frequency} };
+
+% allocate({[], Allocated}, _Pid) ->
+% 	{{[], Allocated}, {error, no_frequency}};
+
 
 allocate({[Freq|Free], Allocated}, Pid) ->
-	case (count(Pid, Allocated) < 3) of
-		true -> { { Free, [ {Freq, Pid} | Allocated ] }, {ok, Freq} };
-		false  -> { {[Freq|Free], Allocated}, {error, frequencies_already_allocated} }
+	case count(Pid, Allocated) of
+		Count when Count < 3 ->
+			{ { Free, [ {Freq, Pid} | Allocated ] }, {ok, Freq} };
+		_Count ->
+			{ {[Freq|Free], Allocated}, {error, frequencies_already_allocated} }
 	end.
 
 
-deallocate({Free, []}, _) ->
-	{ {Free, []}, {error, no_used_frequences} };
+deallocate({_Free, [] = _Allocated} = Frequencies, _Freq, _Pid) ->
+    { Frequencies, {error, no_used_frequences} };
 
-deallocate({Free, Allocated}, Freq) ->
+% deallocate({Free, []}, _) ->
+% 	{ {Free, []}, {error, no_used_frequences} };
+
+deallocate({Free, Allocated} = Frequencies, Freq, Pid) ->
 	case lists:keyfind(Freq, 1, Allocated) of
-		false -> { {Free, Allocated}, {error, frequence_not_used} };
-		_Other ->
+		false ->
+			{ Frequencies, {error, frequence_not_used} };
+		{Freq, Pid} ->
 			NewAllocated = lists:keydelete(Freq, 1, Allocated),
-			{ { [Freq|Free], NewAllocated }, ok}
+			{ { [Freq|Free], NewAllocated }, ok};
+		_Other ->
+			{Frequencies, {error, access_denied}}
 	end.
 
 
@@ -92,26 +112,3 @@ count(Needle, Haystack) -> count(Needle, Haystack, 0).
 count(_, [], Count) -> Count;
 count(X, [{_, X}|Rest], Count) -> count(X, Rest, Count+1);
 count(X, [_|Rest], Count) -> count(X, Rest, Count).
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
