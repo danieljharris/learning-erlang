11e3780cba7c34f00dafd4b587092a2f47828e6d
danieljharris <danieljharris26@yahoo.co.uk>
2017-08-18 16:49:56 +0100

    Updated call_count to kinda work
    Still have no idea what the book means by using macros to count function calls to modules, but made a normal program that does it without macros

--- Chapter7/call_count.erl
+++ Chapter7/call_count.erl
@@ -1,13 +1,6 @@
 -module(call_count).
--export([test/0]).
-
--define(COUNT(N), N+1)
-
--define(SHOW_EVAL(Expression), io:format("~p = ~p~n",[??Expression,Expression])).
-
-test() -> ?SHOW_EVAL(length([1,2,3,4,5])).
-
-
+-export([count/2]).
+-export([counter/1]).
 
 
 % Exercise 7-7: Counting Calls
@@ -16,3 +9,45 @@ test() -> ?SHOW_EVAL(length([1,2,3,4,5])).
 % No idea how to go about doing this? Does it want me to give it a file name of a file with erlang code
 % in it and then search the file for every call to a module? Or does it want me to make something that
 % checks how many times a module is called from the current module?
+
+% I just decided to make a program that given a file will count the ammount of words (Which can be "Module:Function")
+
+%%Example:
+% call_count:count("/Users/daniel.harris/Documents/GitErlang/learning-erlang/Chapter7/call_count.erl", "file:close").
+
+count(File, ModFun) ->
+   register(counter, spawn(call_count, counter, [0])),
+
+
+   {ok, OpenFile} = file:open(File, read),
+   read_file(OpenFile, ModFun),
+
+   whereis(counter)!{stat,self()},
+   receive {count, Ammount} -> true end,
+   Ammount.
+
+
+%% counter
+counter(Accumulator) ->
+   receive 
+      {stat, Pid} -> Pid!{count,Accumulator};
+
+      Count -> counter(Accumulator + Count)
+   end.
+
+
+%% read_file
+read_file(OpenFile, Word) ->
+   case io:get_line(OpenFile, "") of
+      eof -> file:close(OpenFile);
+      Line -> 
+         read_line(Line, Word), 
+         read_file(OpenFile, Word)
+   end.
+
+%% read_line
+read_line(Line, Word) ->
+   case re:run(Line, Word, [global]) of
+      {match, MatchedList} -> counter ! length(MatchedList);
+      _ -> ok
+   end.
