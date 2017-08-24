2fa042e0e42e8f9201969ff6e04d4ea3fb7d673d
danieljharris <danieljharris26@yahoo.co.uk>
2017-08-24 16:49:13 +0100

    Updated to work with macro

--- Chapter7/call_count.erl
+++ Chapter7/call_count.erl
@@ -1,53 +1,69 @@
 -module(call_count).
--export([count/2]).
+-export([test/0]).
 -export([counter/1]).
+
+-define(COUNT_CALL(Module, Function), (counter ! {count, Module, Function} )).
 
 
 % Exercise 7-7: Counting Calls
-% How can you use the Erlang macro facility to count the number of calls to a particular function in a particular module?
 
-% No idea how to go about doing this? Does it want me to give it a file name of a file with erlang code
-% in it and then search the file for every call to a module? Or does it want me to make something that
-% checks how many times a module is called from the current module?
+test() ->
+  counter_start(),
 
-% I just decided to make a program that given a file will count the ammount of words (Which can be "Module:Function")
+  foo(),
+  bar(),
+  foo(),
+  foo(),
+  bar(),
 
-%%Example:
-% call_count:count("/Users/daniel.harris/Documents/GitErlang/learning-erlang/Chapter7/call_count.erl", "file:close").
-
-count(File, ModFun) ->
-   register(counter, spawn(call_count, counter, [0])),
-
-
-   {ok, OpenFile} = file:open(File, read),
-   read_file(OpenFile, ModFun),
-
-   whereis(counter)!{stat,self()},
-   receive {count, Ammount} -> true end,
-   Ammount.
+  Result = counter_read(),
+  counter_stop(),
+  Result.
 
 
-%% counter
+% Example Functions
+foo() ->
+  ?COUNT_CALL(call_count, foo),
+  ok.
+
+bar() ->
+  ?COUNT_CALL(call_count, bar),
+  ok.
+
+
+
+% Counting Functions
+counter_start() ->
+  register(counter, spawn(call_count, counter, [ [] ])),
+  ok.
+
+counter_stop() ->
+  counter ! stop.
+
+counter_read() ->
+  counter ! {read, self()},
+  receive
+    Reply -> Reply
+  after 1000 -> {error, timeout}
+  end.
+
+% Counter Loop
 counter(Accumulator) ->
    receive 
-      {stat, Pid} -> Pid!{count,Accumulator};
+      {read, Pid} ->
+        Pid!Accumulator,
+        counter(Accumulator);
 
-      Count -> counter(Accumulator + Count)
-   end.
+      {count, Module, Function} ->
+        case lists:keyfind({Module, Function}, 1, Accumulator) of
+          false ->
+            counter([{{Module, Function}, 1} | Accumulator]);
 
+          {_, Count} -> 
+            NewList = lists:keydelete({Module, Function}, 1, Accumulator),
+            counter([{{Module, Function}, Count + 1} | NewList])
+        end;
 
-%% read_file
-read_file(OpenFile, Word) ->
-   case io:get_line(OpenFile, "") of
-      eof -> file:close(OpenFile);
-      Line -> 
-         read_line(Line, Word), 
-         read_file(OpenFile, Word)
-   end.
-
-%% read_line
-read_line(Line, Word) ->
-   case re:run(Line, Word, [global]) of
-      {match, MatchedList} -> counter ! length(MatchedList);
-      _ -> ok
+      stop ->
+        ok
    end.
