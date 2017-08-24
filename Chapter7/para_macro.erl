87f00df8e4da01307d0c804b17343d1dd34e1dc5
danieljharris <danieljharris26@yahoo.co.uk>
2017-08-24 16:16:40 +0100

    Update para_macro.erl

--- Chapter7/para_macro.erl
+++ Chapter7/para_macro.erl
@@ -4,9 +4,9 @@
 % Exercise 7-6: Parameterized Macros
 
 -ifdef(show).
-	-define(SHOW_EVAL(Expression), io:format("~p = ~p~n",[??Expression,Expression])).
+  -define(SHOW_EVAL(Expression), Result = Expression, io:format("~p = ~p~n",[??Expression,Result]), Result).
 -else.
-	-define(SHOW_EVAL(Expression), io:format("~p~n",[Expression])).
+  -define(SHOW_EVAL(Expression), Expression).
 -endif.
 
 test() -> ?SHOW_EVAL(length([1,2,3,4,5])).
