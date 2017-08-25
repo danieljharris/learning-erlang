5028a6fc21fcada5562962f1f4c7e82518e610b8
danieljharris <danieljharris26@yahoo.co.uk>
2017-08-25 15:51:18 +0100

    Update trees.erl

--- Chapter9/trees.erl
+++ Chapter9/trees.erl
@@ -1,26 +1,26 @@
 -module(trees).
--export([ex_tree/0, treeToList/1, listToTree/1, deserialize/1]).
+-export([example_tree/0, treeToList/1, listToTree/1, deserialize/1, list_serialization/1, binary_deserialization/1]).
 
 -vsn(1.0).
 
-ex_tree() ->
-	{node,
-		{node,
-			{leaf,car},
-			{node,
-				{leaf,dog},
-				{leaf,emu}
-			}
-		},
-		{leaf,fish}
-	}.
+example_tree() ->
+  {node,
+    {node,
+      {leaf,car},
+      {node,
+        {leaf,dog},
+        {leaf,emu}
+      }
+    },
+    {leaf,fish}
+  }.
 
 treeToList({leaf,N}) ->
-	[2,N];
+  [2,N];
 treeToList({node,T1,T2}) ->
-	TTL1 = treeToList(T1), [Size1|_] = TTL1,
-	TTL2 = treeToList(T2), [Size2|List2] = TTL2,
-	[Size1+Size2|TTL1++List2].
+  TTL1 = treeToList(T1), [Size1|_] = TTL1,
+  TTL2 = treeToList(T2), [Size2|List2] = TTL2,
+  [Size1+Size2|TTL1++List2].
 
 
 
@@ -28,6 +28,12 @@ deserialize([_|Ls]) -> listToTree(Ls).
 
 listToTree([2,N]) -> {leaf,N};
 listToTree([N]) -> {leaf,N};
-listToTree([M|Rest] = Code) -> 
-	{Code1,Code2} = lists:split(M-1,Rest),
-	{node, listToTree(Code1), listToTree(Code2)}.
+listToTree([M|Rest] = _Code) -> 
+  {Code1,Code2} = lists:split(M-1,Rest),
+  {node, listToTree(Code1), listToTree(Code2)}.
+
+
+
+% Exercise 9-6: Bitstrings
+list_serialization(List) -> erlang:term_to_binary(List).
+binary_deserialization(Binary) -> erlang:binary_to_term(Binary).
