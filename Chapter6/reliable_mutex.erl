5100ae378e4adbce5e23845cea519774de045fe7
danieljharris <danieljharris26@yahoo.co.uk>
2017-08-25 09:50:56 +0100

    Update reliable_mutex.erl

--- Chapter6/reliable_mutex.erl
+++ Chapter6/reliable_mutex.erl
@@ -1,129 +1,96 @@
 -module(reliable_mutex).
+-export([test/0]).
 -export([start/0, stop/0]).
 -export([wait/0, signal/0]).
--export([init/0]).
+-export([init/0, start_wait/0, start_wait_crash/0]).
 
 % Exercise 6-2: A Reliable Mutex Semaphore
 
+test() ->
+  catch start(),
+  spawn(reliable_mutex, start_wait_crash, []),
+  spawn(reliable_mutex, start_wait, []),
+  spawn(reliable_mutex, start_wait, []),
+  spawn(reliable_mutex, start_wait, []),
+  ok.
 
-%% Link method
-% msg(Message) ->
-% 	process_flag(trap_exit, true),
-% 	(catch link(whereis(cell))),
-% 	cell ! {Message, self()},
-% 	receive
-% 		{'EXIT', _Pid, Reason} ->
-% 			stop(),
-% 			{error, Reason};
-% 		{reply, Reply} ->
-% 			unlink(whereis(cell)),
-% 			Reply
-% 	end.
+start_wait_crash() ->
+  io:format("~p: waiting~n", [self()]),
+  wait(),
+  io:format("~p: crashing~n", [self()]).
 
-%% Monitor method
-% msg(Message) ->
-% 	process_flag(trap_exit, true),
-% 	Reference = erlang:monitor(process, whereis(cell)),
-% 	cell ! {Message, self()},
-% 	receive
-% 		{'DOWN',Reference,process, _Pid, Reason} ->
-% 			stop(),
-% 			Reason;
-% 		{reply, Reply} ->
-% 			erlang:demonitor(Reference),
-% 			Reply
-% 	end.
-
-
+start_wait() ->
+  io:format("~p: waiting~n", [self()]),
+  wait(),
+  io:format("~p: finished~n", [self()]),
+  signal().
 
 start() ->
-	register(mutex, spawn(reliable_mutex, init, [])).
+  register(mutex, spawn(reliable_mutex, init, [])).
 
 stop() ->
-	mutex ! stop.
+  mutex ! stop.
 
 wait() ->
-	mutex ! {wait, self()},
-	receive ok -> ok end.
+  mutex ! {wait, self()},
+  receive ok -> ok end.
 
 signal() ->
-	mutex ! {signal, self()}, ok.
-
-
+  mutex ! {signal, self()}, ok.
 
 init() ->
-	process_flag(trap_exit, true),
-	free().
+  process_flag(trap_exit, true),
+  free().
 
 
-% States: free, busy, terminate
 free() ->
-	receive
-		{wait, Pid} ->
-			case process_info(Pid) of
-				undefined ->
-					free();
-				_ ->
-					Pid ! ok,
-					busy(Pid)
-			end;
-		stop ->
-			terminate()
-	end.
+  receive
+    {wait, Pid} ->
+        Pid ! ok,
+        busy(Pid);
+    stop ->
+      terminate()
+  end.
 
 
-%% Link method
+%% Link method ----------------------------
 busy(Pid) ->
+  try link(Pid) of
+    true -> ok
+  catch
+    _:_ -> free()
+  end,
 
-	try link(Pid) of
-		true -> ok
-	catch
-		_:_ -> {error, could_not_link}
-	end,
+  receive
+    {signal, Pid} ->
+      unlink(Pid),
+      free();
 
+    {'EXIT', _Pid, _Reason} ->
+      unlink(Pid),
+      free()
+  end.
+%% Link method ----------------------------
 
-	receive
-		{signal, Pid} ->
-			try unlink(Pid) of
-				true -> ok
-			catch
-				_:_ -> {error, could_not_un_link}
-			end,
-			free();
-
-
-		{'EXIT', _Pid, _Reason} ->
-			try unlink(Pid) of
-				true -> ok
-			catch
-				_:_ -> {error, could_not_un_link}
-			end,
-			terminate()
-	end.
-
-%% Monitor method
+%% Monitor method -------------------------
 % busy(Pid) ->
-% 	Reference = erlang:monitor(process, whereis(cell)),
-% 	receive
-% 		{signal, Pid} ->
-% 			erlang:demonitor(Reference),
-% 			free();
+%   Reference = erlang:monitor(process, whereis(cell)),
+%   receive
+%     {signal, Pid} ->
+%       erlang:demonitor(Reference),
+%       free();
 
-% 		{'DOWN', _Reference, process, _Pid, _Reason} ->
-% 			erlang:demonitor(Reference),
-% 			terminate()
-% 	end.
-
+%     {'DOWN', _Reference, process, _Pid, _Reason} ->
+%       erlang:demonitor(Reference),
+%       free()
+%   end.
+%% Monitor method -------------------------
 
 
 terminate() ->
-	receive
-		{wait, Pid} ->
-			exit(Pid, kill),
-			terminate()
-	after 0 -> ok
-	end.
-
-
-
-
+  receive
+    {wait, Pid} ->
+      exit(Pid, kill),
+      terminate()
+  after 0 -> ok
+  end.
