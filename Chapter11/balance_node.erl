f5304ee2daf5a7fe43c464aac9662cee8eb20027
danieljharris <danieljharris26@yahoo.co.uk>
2017-08-29 10:41:31 +0100

    Finished Chapter11

--- Chapter11/balance_node.erl
+++ Chapter11/balance_node.erl
@@ -8,7 +8,11 @@
 % Exercise 11-1: Distributed Associative Store (Main Nodes)
 
 start() ->
+	event_manager:start(node_logger, [{log_handler, "NodeLog"}]),
 	register(balancer, spawn(balance_node, balancer, [?NODE_ONE])).
+
+stop() ->
+	event_manager:stop(node_logger).
 
 
 balancer(LastNode) ->
@@ -23,10 +27,16 @@ balancer(LastNode) ->
 			monitor_node(Node, true),
 			balancer(NextNode);
 
-		{add, Address, Nickname}	-> {address_loop, NextNode} ! {add, Address, Nickname}, balancer(NextNode);
-		{remove, Address}					-> {address_loop, NextNode} ! {remove, Address}, balancer(NextNode);
+		{add, Address, Nickname}	-> {address_loop, NextNode} ! {add, Address, Nickname},
+			event_manager:send_event(node_logger, {add, {Address, Nickname}, NextNode}),
+			balancer(NextNode);
+
+		{remove, Address}					-> {address_loop, NextNode} ! {remove, Address},
+			event_manager:send_event(node_logger, {remove, {Address}, NextNode}),
+			balancer(NextNode);
 		
 		{lookup, Address, Pid}		->
+			event_manager:send_event(node_logger, {lookup, {Address}, NextNode}),
 			clear_mailbox(),
 			{address_loop, NextNode} ! {lookup, Address, self()},
 			receive
@@ -36,10 +46,17 @@ balancer(LastNode) ->
 			balancer(NextNode);
 
 		% Restarts crashed nodes
-		{nodedown, ?DB_NODE} -> spawn(?DB_NODE, db_node, start, []), balancer(NextNode);
-		{nodedown, Node} -> spawn(Node, das_node, create, []), balancer(NextNode);
+		{nodedown, ?DB_NODE} ->
+			event_manager:send_event(node_logger, {db_down, ?DB_NODE, restarting}),
+			spawn(?DB_NODE, db_node, start, []),
+			balancer(NextNode);
 
-		stop -> ok
+		{nodedown, Node} ->
+			event_manager:send_event(node_logger, {das_down, Node, restarting}),
+			spawn(Node, das_node, create, []),
+			balancer(NextNode);
+
+		stop -> stop(), ok
 	end.
 
 clear_mailbox() ->
