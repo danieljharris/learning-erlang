adef8a91de6d5f539e506eae34653691bb37d1e3
danieljharris <danieljharris26@yahoo.co.uk>
2017-09-06 13:43:31 +0100

    Fixed 17-2

--- Chapter17/gc_dbg.erl
+++ Chapter17/gc_dbg.erl
@@ -1,47 +1,66 @@
 -module(gc_dbg).
 -export([measure/3, averageT/1, averageNT/1]).
--export([loop/0]).
+-export([do_measure/3]).
 
 % Exercise 17-2: Garbage Collection Using dbg
 
-
 measure(Module, Function, Args) ->
-  register(test ,spawn(Module, Function, [Args])),
-
-  HandlerFun =
-    fun
-
-      ({trace, Pid, gc_minor_start, Start}, _) ->
-        Start;
-      ({trace, Pid, gc_minor_end, End}, Start) ->
-        {_, {_,OHS}} = lists:keysearch(old_heap_size, 1, Start),
-        {_, {_,OHE}} = lists:keysearch(old_heap_size, 1, End),
-        io:format("Old heap size delta after gc:~w~n",[OHS-OHE]),
-        {_, {_,HS}} = lists:keysearch(heap_size, 1, Start),
-        {_, {_,HE}} = lists:keysearch(heap_size, 1, End),
-        io:format("Heap size delta after gc:~w~n",[HS-HE])
-    end,
-
-  dbg:tracer(process, {HandlerFun, null}).
-  dbg:p(self(), [garbage_collection]).
-
-  % List = lists:seq(1,1000).
-  % RevList = lists:reverse(List).
-
-  test ! start,
+  spawn(gc_dbg, do_measure, [Module, Function, Args]),
   ok.
 
+do_measure(Module, Function, Args) ->
+  HandlerFun =
+    fun
+      % Minor -------------------
+      ({trace, _, gc_minor_start, Start}, _) ->
+        {minor, erlang:timestamp(), Start};
 
-loop() ->
-  receive
-    Message ->
-      io:format("I traced: ~p~n", [Message]),
-      loop()
-  after 5000 ->
-    io:format("Error: ~p~n", [{error, timeout}]),
-    {error, timeout}
-  end.
+      ({trace, _, gc_minor_end, End}, {minor, IStart, IStartData}) ->
+        Now = erlang:timestamp(),
 
+        % Old Heap
+        {_, {_,OHS}} = lists:keysearch(old_heap_size, 1, IStartData),
+        {_, {_,OHE}} = lists:keysearch(old_heap_size, 1, End),
+        % Heap
+        {_, {_,HS}} = lists:keysearch(heap_size, 1, IStartData),
+        {_, {_,HE}} = lists:keysearch(heap_size, 1, End),
+
+        display_gc({minor, IStart}, Now, OHS-OHE, HS-HE),
+        ok;
+      % Minor -------------------
+
+      % Major -------------------
+      ({trace, _, gc_major_start, Start}, _) ->
+        {major, erlang:timestamp(), Start};
+
+      ({trace, _, gc_major_end, End}, {major, JStart, JStartData}) ->
+        Now = erlang:timestamp(),
+
+        % Old Heap
+        {_, {_,OHS}} = lists:keysearch(old_heap_size, 1, JStartData),
+        {_, {_,OHE}} = lists:keysearch(old_heap_size, 1, End),
+        % Heap
+        {_, {_,HS}} = lists:keysearch(heap_size, 1, JStartData),
+        {_, {_,HE}} = lists:keysearch(heap_size, 1, End),
+
+        display_gc({major, JStart}, Now, OHS-OHE, HS-HE),
+        ok
+      % Major -------------------
+
+    end,
+
+  dbg:tracer(process, {HandlerFun, null}),
+  dbg:p(self(), [garbage_collection]),
+
+  Result = apply(Module, Function, [Args]),
+  dbg:stop(),
+  Result.
+
+
+display_gc({Type, Start}, End, OldHeap, Heap) ->
+  io:format("Gc ~p (Old Heap) freed: ~p~n", [Type, OldHeap]),
+  io:format("Gc ~p (Heap) freed: ~p~n", [Type, Heap]),
+  io:format("Gc ~p took: ~p mircoseconds~n", [Type, timer:now_diff(End, Start)]).
 
 % Tail-recursive function
 averageT(List) -> sum(List) / len(List).
--- Chapter17/mgct.erl
+++ Chapter17/mgct.erl
@@ -2,11 +2,10 @@
 -export([measure/3, averageT/1, averageNT/1]).
 -export([loop/2]).
 
-
 % Exercise 17-1: Measuring Garbage Collection Times
 
 measure(Module, Function, Args) ->
-  LoopPid = spawn(mgct, loop, [{minor, 0, 0}, {major, 0, 0}]),
+  LoopPid = spawn(mgct, loop, [{minor, 0}, {major, 0}]),
 
   Pid = spawn(Module, Function, [Args]),
   erlang:trace(Pid, true, [garbage_collection, {tracer, LoopPid}]),
@@ -17,31 +16,34 @@ loop(Minor, Major) ->
   receive
     % Minor -------------------
     {trace, _, gc_minor_start, _} ->
-      {minor, _IStart, IEnd} = Minor,
-      loop({minor, erlang:timestamp(), IEnd}, Major);
+      {minor, _IStart} = Minor,
+      loop({minor, erlang:timestamp()}, Major);
 
     {trace, _, gc_minor_end, _} ->
-      {minor, IStart, _IEnd} = Minor,
-      loop({minor, IStart, erlang:timestamp()}, Major);
-    
+      Now = erlang:timestamp(),
+      {minor, IStart} = Minor,
+      display_gc({minor, IStart}, Now),
+      loop({minor, IStart}, Major);
+    % Minor -------------------
+
     % Major -------------------
     {trace, _, gc_major_start, _} ->
-      {major, _JStart, JEnd} = Major,
-      loop(Minor, {major, erlang:timestamp(), JEnd});
+      {major, _JStart} = Major,
+      loop(Minor, {major, erlang:timestamp()});
 
     {trace, _, gc_major_end, _} ->
-      {major, JStart, _JEnd} = Major,
       Now = erlang:timestamp(),
-      display_gc(Minor, {major, JStart, Now}),
-      loop(Minor, {major, JStart, Now})
+      {major, JStart} = Major,
+      display_gc({major, JStart}, Now),
+      loop(Minor, {major, JStart})
+    % Major -------------------
 
   after 5000 ->
-    {error, timeout}
+    {error, trace_timeout}
   end.
 
-display_gc({minor, IStart, IEnd}, {major, JStart, JEnd}) ->
-  io:format("Gc minor took: ~p mircoseconds~n", [timer:now_diff(IEnd, IStart)]),
-  io:format("Gc major took: ~p mircoseconds~n", [timer:now_diff(JEnd, JStart)]).
+display_gc({Type, Start}, End) ->
+  io:format("Gc ~p took: ~p mircoseconds~n", [Type, timer:now_diff(End, Start)]).
 
 
 
